1ec998790282134301cf1e464bc57bcd78b50c19
Alex <aleks.rk@gmail.com>
2018-04-14 01:56:41 +0300

    Added logs and history endpoints

--- src/models/API.ts
+++ src/models/API.ts
@@ -1,8 +1,11 @@
 import PlantWateringSystem from "../PlantWateringSystem";
 import SensorInterface from "./sensors/SensorInterface";
 import SensorMeasurement from "./sensors/SensorMeasurement";
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const glob = require("glob");
+
 export default class API {
     pws: PlantWateringSystem;
     api: any;
@@ -22,6 +25,14 @@ export default class API {
                     {
                         text: 'Get current sensor status information',
                         href : "/sensors"
+                    },
+                    {
+                        text: 'Get logs',
+                        href : "/logs"
+                    },
+                    {
+                        text: 'Get historical measurements from sensors',
+                        href : "/history"
                     }
                 ]
             });
@@ -33,6 +44,18 @@ export default class API {
             });
             res.json({ data });
         }.bind(this));
+        this.api.get("/logs", function(req: any, res: any) {
+            glob("logs/*.log", function (err: Error, files: Array<any>) {
+                res.json({ data: files });
+            });
+        }.bind(this));
+        this.api.get("/history", function(req: any, res: any) {
+            glob("logs/*.csv", function (err: Error, files: Array<any>) {
+                res.json({ data: files });
+            });
+        }.bind(this));
+        this.api.use('/logs', express.static('logs'))
+        this.api.use('/history', express.static('logs'))
 
         var server = this.api.listen(3000, function () {
             console.log("Listening on port %s...", server.address().port);
